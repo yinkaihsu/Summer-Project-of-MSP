d39948fef8f51af2799cf323497e2caaa14042e7
yinkaihsu <yinkaihsu@gmail.com>
2018-01-22 01:26:33 +0800

    Python crawler of PTT beauty
    The files contain all of the information about Python crawler of PTT beauty.

---
+++ url_downloader_of_PTT_crawler.py
@@ -0,0 +1,39 @@
+import unittest
+import codecs, json, os
+import time
+import re,os,sys
+
+# Put your JSON file path of PTT_crawler in Beauty here
+FilePath = "your JSON file path"
+
+def get_contents(JsonPath):
+    with open(JsonPath, 'r',encoding = 'utf-8') as f:
+        s = f.read()
+        data = json.loads(s)
+    contents = []
+    titles = []
+    article_ids = []
+    for article in data['articles']:
+        contents.append(article['content'])
+        titles.append(article['article_title'])
+        article_ids.append(article['article_id'])
+    return(contents,titles,article_ids)
+
+contents,titles,article_ids = get_contents(FilePath)
+#contents
+
+index = 0
+Beauty_images_urls = []
+for content in contents:
+    if titles[index].startswith('[正妹]'):
+        Beauty_images_urls.append(re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', content))
+    index = index + 1
+#Beauty_images_urls
+
+# Flatten the list of Beauty_images_urls
+Beauty_images_urls_list = []
+for x in Beauty_images_urls:
+    for y in x:
+        if y.endswith('.jpg'):
+            Beauty_images_urls_list.append(y)
+#Beauty_images_urls_list
